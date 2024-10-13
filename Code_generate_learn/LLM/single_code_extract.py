00aef28180ccf784a13552d061e5184c372029ba
chengjunhang <23230617@buaa.edu.cn>
2024-10-13 13:58:11 +0800

    commit single_code_extract

---
+++ Code_generate_learn/LLM/single_code_extract.py
@@ -0,0 +1,17 @@
+def process_generation_to_code(gens):
+    if "```python" in gens:
+        gens = gens.split("```python")[1].split("```")[0]
+    elif "```" in gens:
+        gens = gens.split("```")[1].split("```")[0]
+
+    return gens.split("\n")[1:-1]
+
+
+gens = """
+Here is some Python code:
+```python
+def hello_world():
+    print("Hello, world!")
+"""
+
+print(process_generation_to_code(gens))
